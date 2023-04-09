2986f66cb9c2e4a3a8dfa010bb9a71d74c0ac782
dpoliwhi <didenkoev93@gmail.com>
2023-04-09 14:57:57 +0700

    Creating database, environment setting

---
+++ src/main/java/ru/didenko/smartconsulting/seasonalservices/SeasonalServicesApplication.java
@@ -0,0 +1,13 @@
+package ru.didenko.smartconsulting.seasonalservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SeasonalServicesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeasonalServicesApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/didenko/smartconsulting/seasonalservices/SeasonalServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.didenko.smartconsulting.seasonalservices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeasonalServicesApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
