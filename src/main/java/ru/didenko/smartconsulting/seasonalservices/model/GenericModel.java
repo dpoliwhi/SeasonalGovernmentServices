51cf305162fb86d36e0a788e9e57b7d7acf0426d
dpoliwhi <didenkoev93@gmail.com>
2023-04-09 14:59:33 +0700

    create develop branch

--- src/main/java/ru/didenko/smartconsulting/seasonalservices/model/GenericModel.java
+++ src/main/java/ru/didenko/smartconsulting/seasonalservices/model/GenericModel.java
@@ -1,2 +1,39 @@
-package ru.didenko.smartconsulting.seasonalservices.model;public class GenericModel {
+package ru.didenko.smartconsulting.seasonalservices.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@MappedSuperclass
+public class GenericModel {
+    @Id
+    @Column(name = "id", nullable = false)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "default_generator")
+    private Long id;
+
+    @Column(name = "created_when")
+    private LocalDateTime createdWhen;
+
+    @Column(name = "created_by")
+    private String createdBy;
+
+    @Column(name = "updated_when")
+    private LocalDateTime updatedWhen;
+
+    @Column(name = "updated_by")
+    private String updatedBy;
+
+    @Column(name = "is_deleted")
+    private boolean isDeleted;
+
+    @Column(name = "deleted_when")
+    private LocalDateTime deletedWhen;
+
+    @Column(name = "deleted_by")
+    private String deletedBy;
 }
--- src/main/java/ru/didenko/smartconsulting/seasonalservices/model/Service.java
+++ src/main/java/ru/didenko/smartconsulting/seasonalservices/model/Service.java
@@ -1,2 +1,34 @@
-package ru.didenko.smartconsulting.seasonalservices.model;public class Service {
+package ru.didenko.smartconsulting.seasonalservices.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+
+@Entity
+@Table(name = "service")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@SequenceGenerator(name = "default_generator", sequenceName = "service_seq", allocationSize = 1)
+public class Service extends GenericModel {
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "date_start")
+    private LocalDate dateStart;
+
+    @Column(name = "date_expiration")
+    private LocalDate dateExpiration;
+
+    @Column(name = "amount")
+    private Long amount;
+
+    @Column(name = "balance")
+    private Long balance;
 }
